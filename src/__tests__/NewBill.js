ed87268424a2e963acb659084c06390028840555
loic <loicanaelbdf@gmail.com>
2022-07-24 13:41:51 -0700

    added unit tests for NewBill page for icon hightlighted and form input length to equal 9

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -5,14 +5,35 @@
 import { screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import router from "../app/Router.js";
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
+import { waitFor, fireEvent } from "@testing-library/dom"
+import store from "../__mocks__/store"
+
 
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
+    test("Then mail icon in vertical layout should be highlighted", async () => {
+
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.NewBill)
+      await waitFor(() => screen.getByTestId('icon-mail'))
+      const windowIcon = screen.getByTestId('icon-mail')    
+      expect(windowIcon.classList.contains('active-icon')).toBe(true);
+    })
+
+    test('Then, I have form for new bill', () => {
+      document.body.innerHTML = NewBillUI()
+      expect(screen.getByTestId('form-new-bill')).toHaveLength(9)
     })
   })
 })
